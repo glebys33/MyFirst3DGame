9b5496fa3fb52adeded9b27b1f7f1dd41a732b7e
glebys33 <glebys.kiselev@gmail.com>
2021-12-02 19:49:36 +0300

    1 версия игры

---
+++ config.py
@@ -0,0 +1,33 @@
+import math
+
+
+# настройки игры
+WIDTH = 1200
+HEIGHT = 800
+HALF_WIDTH = WIDTH / 2
+HALF_HEIGHT = HEIGHT / 2
+FPS = 60
+TILE = 100
+
+# лучи для 3D
+FOV = math.pi / 3
+HALF_FOV = FOV / 2
+N_RAYS = 120
+MAX_DEPHH = 800
+DELTA_ANGLE = FOV / N_RAYS
+DIST = N_RAYS / (2 * math.tan(HALF_FOV))
+PROJ_COEFF = 3 * DIST * TILE
+SCALE = WIDTH // N_RAYS
+
+# настройки игрока
+player_pos = (HALF_WIDTH, HALF_HEIGHT)
+player_angle = 0
+player_speed = 3
+
+# цвета
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+GRAY = (125, 125, 125)
