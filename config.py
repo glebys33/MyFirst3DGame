bf4abc02b758c77c4071c14ca90faa503e79ec9b
glebys33 <glebys.kiselev@gmail.com>
2021-12-02 21:24:23 +0300

    О - оптимизация новый класс Drawing. Он отображает всё перепись фунции ray_castom

--- config.py
+++ config.py
@@ -8,21 +8,26 @@ HALF_WIDTH = WIDTH / 2
 HALF_HEIGHT = HEIGHT / 2
 FPS = 60
 TILE = 100
+FPS_POS = (WIDTH - 65, 5)
 
 # лучи для 3D
 FOV = math.pi / 3
 HALF_FOV = FOV / 2
-N_RAYS = 120
+N_RAYS = 300
 MAX_DEPHH = 800
 DELTA_ANGLE = FOV / N_RAYS
 DIST = N_RAYS / (2 * math.tan(HALF_FOV))
-PROJ_COEFF = 3 * DIST * TILE
+PROJ_COEFF = DIST * TILE
 SCALE = WIDTH // N_RAYS
+
+# Миникарта
+MAP_SCALE = 5
+MAP_TILE = TILE // MAP_SCALE
 
 # настройки игрока
 player_pos = (HALF_WIDTH, HALF_HEIGHT)
 player_angle = 0
-player_speed = 3
+player_speed = 2
 
 # цвета
 WHITE = (255, 255, 255)
@@ -32,3 +37,5 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 GRAY = (125, 125, 125)
 BROWN = (121, 77, 24)
+SKYBLUE = (34, 113, 179)
+YELOW = (255, 255, 0)
--- main.py
+++ main.py
@@ -14,6 +14,7 @@ class Player:
         return (self.x, self.y)
 
     def movement(self):
+        # Предвижение игрока
         sin_a = math.sin(self.angle)
         cos_a = math.cos(self.angle)
         keys = pygame.key.get_pressed()
@@ -35,30 +36,75 @@ class Player:
             self.angle += 0.02
 
 
+# Отрисовка всего
+class Drawing:
+    def __init__(self, screen, screen_map):
+        self.screen = screen
+        self.screen_map = screen_map
+        self.font = pygame.font.SysFont("Arial", 36, bold=True)
+
+    def background(self):
+        # Отрисовка неба и земли
+        pygame.draw.rect(self.screen, SKYBLUE, (0, 0, WIDTH, HALF_HEIGHT))
+        pygame.draw.rect(self.screen, BROWN, (0, HALF_HEIGHT, WIDTH, HALF_HEIGHT))
+
+    def world(self, player_pos, player_angle):
+        # Отрисовка стен
+        ray_castion(self.screen, player_pos, player_angle)
+
+    def fps(self, clock):
+        # Вывод fps
+        display_fps = str(int(clock.get_fps()))
+        render = self.font.render(display_fps, 0, RED)
+        self.screen.blit(render, FPS_POS)
+
+
+def squareangle(a, b):
+    return (a // TILE) * TILE, (b // TILE) * TILE
+
+
+# Функция каторая отрисовавает стены
 def ray_castion(screen, player_pos, player_angle):
+    ox, oy = player_pos
+    xm, ym = squareangle(ox, oy)
     cur_angle = player_angle - HALF_FOV
-    xo, yo = player_pos
     for ray in range(N_RAYS):
         sin_a = math.sin(cur_angle)
         cos_a = math.cos(cur_angle)
-        for depth in range(MAX_DEPHH):
-            x = xo + depth * cos_a
-            y = yo + depth * sin_a
-            if (x // TILE * TILE, y // TILE * TILE) in world_map:
-                depth *= math.cos(player_angle - cur_angle)
-                proj_height = PROJ_COEFF / depth
-                c = 255 / (1 + depth * depth * 0.0001)
-                color = (c, c, c)
-                pygame.draw.rect(screen, color, (ray * SCALE, HALF_HEIGHT - proj_height // 2, SCALE, proj_height))
+        # Алгоритм для вертикалий
+        x, dx = (xm + TILE, 1) if cos_a >= 0 else (xm, -1)
+        for i in range(0, WIDTH, TILE):
+            depth_v = (x - ox) / cos_a
+            y = oy + depth_v * sin_a
+            if squareangle(x + dx, y) in world_map:
                 break
+            x += dx * TILE
+        # Алгоритм для горизонталий
+        y, dy = (ym + TILE, 1) if sin_a >= 0 else (ym, -1)
+        for i in range(0, HEIGHT, TILE):
+            depth_h = (y - oy) / sin_a
+            x = ox + depth_h * cos_a
+            if squareangle(x, y + dy) in world_map:
+                break
+            y += dy * TILE
+
+        # Выбор точки пересечения
+        depth = depth_v if depth_v < depth_h else depth_h
+        depth *= math.cos(player_angle - cur_angle)
+        proj_height = PROJ_COEFF / depth
+        c = 255 / (1 + depth * depth * 0.0001)
+        color = (c, c//2, c//3)
+        pygame.draw.rect(screen, color, (ray * SCALE, HALF_HEIGHT - proj_height // 2, SCALE, proj_height))
         cur_angle += DELTA_ANGLE
 
 
 if __name__ == "__main__":
     pygame.init()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    screen_map = pygame.Surface((WIDTH // MAP_SCALE, HEIGHT // MAP_SCALE))
     clock = pygame.time.Clock()
     player = Player()
+    drawer = Drawing(screen, screen_map)
 
     while True:
         for event in pygame.event.get():
@@ -67,10 +113,10 @@ if __name__ == "__main__":
         player.movement()
         screen.fill(BLACK)
 
-        pygame.draw.rect(screen, BLUE, (0, 0, WIDTH, HALF_HEIGHT))
-        pygame.draw.rect(screen, BROWN, (0, HALF_HEIGHT, WIDTH, HALF_HEIGHT))
-
-        ray_castion(screen, player.pos, player.angle)
+        drawer.background()
+        drawer.world(player.pos, player.angle)
+        drawer.fps(clock)
+        drawer.mini_map(player)
 
         pygame.display.flip()
-        clock.tick(FPS)
+        clock.tick()
